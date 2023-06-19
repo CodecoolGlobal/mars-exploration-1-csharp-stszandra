62707a2fba75728ad7f869578aacb705d896488b
fehervendel <fehervendel@gmail.com>
2023-06-19 14:34:20 +0200

    feature: implement tests

--- Codecool.MarsExploration/Configuration/Service/IMapConfigurationValidator.cs
+++ Codecool.MarsExploration/Configuration/Service/IMapConfigurationValidator.cs
@@ -5,4 +5,13 @@ namespace Codecool.MarsExploration.Configuration.Service;
 public interface IMapConfigurationValidator
 {
     bool Validate(MapConfiguration mapConfig);
+    public bool IsDimensionGrowthOk(MapConfiguration mapConfig);
+
+    public bool IsPreferredPlacementOk(MapConfiguration mapConfig);
+
+    public bool DoSymbolAndNameMatch(MapConfiguration mapConfig);
+
+    public bool SingleOrMultiDimensionalOk(MapConfiguration mapConfig);
+
+    public bool TotalNumberOfElementsOk(MapConfiguration mapConfig);
 }

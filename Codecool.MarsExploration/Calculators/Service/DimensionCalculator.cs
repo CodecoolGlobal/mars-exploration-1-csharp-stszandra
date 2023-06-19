00b066b81ace354a4b9de61d95f687b1839e4e61
stszandra <snszandra@gmail.com>
2023-06-19 18:21:44 +0200

    feature: create DimensionCalculator and implement logic of CalculateDimension method

---
+++ Codecool.MarsExploration/Calculators/Service/DimensionCalculator.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Codecool.MarsExploration.Calculators.Service;
+
+public class DimensionCalculator : IDimensionCalculator
+{
+    public int CalculateDimension(int size, int dimensionGrowth)
+    {
+        double squareRootOfSize = Math.Sqrt(size);
+        int minDimension = (int)Math.Ceiling(squareRootOfSize);
+        return minDimension + dimensionGrowth;
+    }
+}

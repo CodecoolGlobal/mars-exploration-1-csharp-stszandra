a96b6d1ef057fadbb5bd903f4139c395a4df5119
fehervendel <fehervendel@gmail.com>
2023-06-19 10:21:54 +0200

    feature: create mapConfiguration objects

--- Codecool.MarsExploration/Program.cs
+++ Codecool.MarsExploration/Program.cs
@@ -50,6 +50,22 @@ internal class Program
             new ElementToSize(1, 30),
         }, 3);
 
+        var pitCfg = new MapElementConfiguration(pitSymbol, "pit", new[]
+        {
+            new ElementToSize(1, 5),
+            new ElementToSize(1, 10),
+        }, 10);
+
+        var mineralCfg = new MapElementConfiguration(mineralSymbol, "mineral", new[]
+        {
+            new ElementToSize(10, 1)
+        }, 0, mountainSymbol);
+
+        var waterCfg = new MapElementConfiguration(waterSymbol, "water", new[]
+        {
+            new ElementToSize(8, 1)
+        }, 0, pitSymbol);
+        
         List<MapElementConfiguration> elementsCfg = new() { mountainsCfg };
         return new MapConfiguration(1000, 0.5, elementsCfg);
     }

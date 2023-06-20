2730073a95ea7ff80e4a149a1d6c732af36b9581
stszandra <snszandra@gmail.com>
2023-06-20 11:47:17 +0200

    feature: implement first GetAdjacentCoordinates method in CoordinateCalculator

--- Codecool.MarsExploration/Calculators/Service/CoordinateCalculator.cs
+++ Codecool.MarsExploration/Calculators/Service/CoordinateCalculator.cs
@@ -15,7 +15,47 @@ public class CoordinateCalculator : ICoordinateCalculator
 
     public IEnumerable<Coordinate> GetAdjacentCoordinates(Coordinate coordinate, int dimension)
     {
-        
+        Coordinate rightNeighbour = null;
+        if (coordinate.X != dimension - 1)
+        {
+            rightNeighbour = coordinate with { X = coordinate.X + 1 };
+        }
+        Coordinate leftNeighbour = null;
+        if (coordinate.X != 0)
+        {
+            rightNeighbour = coordinate with {X = coordinate.X - 1};
+        }
+        Coordinate topNeighbour = null;
+        if (coordinate.X != 0)
+        {
+            topNeighbour = coordinate with {Y = coordinate.Y - 1};
+        }
+        Coordinate bottomNeighbour = null;
+        if (coordinate.X != 0)
+        {
+            bottomNeighbour = coordinate with {Y = coordinate.Y + 1};
+        }
+
+        //IEnumerable<Coordinate> adjacentCoordinates = new List<Coordinate>();
+        List<Coordinate> adjacentCoordinates = new List<Coordinate>();
+        if (rightNeighbour != null)
+        {
+            adjacentCoordinates.Add(rightNeighbour); 
+        }
+        if (leftNeighbour != null)
+        {
+            adjacentCoordinates.Add(leftNeighbour); 
+        }
+        if (topNeighbour != null)
+        {
+            adjacentCoordinates.Add(topNeighbour); 
+        }
+        if (bottomNeighbour != null)
+        {
+            adjacentCoordinates.Add(bottomNeighbour); 
+        }
+
+        return adjacentCoordinates;
     }
 
     public IEnumerable<Coordinate> GetAdjacentCoordinates(IEnumerable<Coordinate> coordinates, int dimension)

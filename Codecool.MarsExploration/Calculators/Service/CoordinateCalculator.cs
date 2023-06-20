bca1448c6731b66dbf7b56c741e8f31a4b0df304
stszandra <snszandra@gmail.com>
2023-06-20 14:51:23 +0200

    refactor: simplify calculation in GetAdjacentCoordinates

--- Codecool.MarsExploration/Calculators/Service/CoordinateCalculator.cs
+++ Codecool.MarsExploration/Calculators/Service/CoordinateCalculator.cs
@@ -15,44 +15,22 @@ public class CoordinateCalculator : ICoordinateCalculator
 
     public IEnumerable<Coordinate> GetAdjacentCoordinates(Coordinate coordinate, int dimension)
     {
-        Coordinate rightNeighbour = null;
+        List<Coordinate> adjacentCoordinates = new List<Coordinate>();
         if (coordinate.X != dimension - 1)
         {
-            rightNeighbour = coordinate with { X = coordinate.X + 1 };
+            adjacentCoordinates.Add(coordinate with { X = coordinate.X + 1 }); 
         }
-        Coordinate leftNeighbour = null;
         if (coordinate.X != 0)
         {
-            leftNeighbour = coordinate with {X = coordinate.X - 1};
+            adjacentCoordinates.Add(coordinate with {X = coordinate.X - 1});
         }
-        Coordinate topNeighbour = null;
         if (coordinate.Y != 0)
         {
-            topNeighbour = coordinate with {Y = coordinate.Y - 1};
+            adjacentCoordinates.Add(coordinate with {Y = coordinate.Y - 1});
         }
-        Coordinate bottomNeighbour = null;
         if (coordinate.Y != dimension - 1)
         {
-            bottomNeighbour = coordinate with {Y = coordinate.Y + 1};
-        }
-
-        //IEnumerable<Coordinate> adjacentCoordinates = new List<Coordinate>();
-        List<Coordinate> adjacentCoordinates = new List<Coordinate>();
-        if (rightNeighbour != null)
-        {
-            adjacentCoordinates.Add(rightNeighbour); 
-        }
-        if (leftNeighbour != null)
-        {
-            adjacentCoordinates.Add(leftNeighbour); 
-        }
-        if (topNeighbour != null)
-        {
-            adjacentCoordinates.Add(topNeighbour); 
-        }
-        if (bottomNeighbour != null)
-        {
-            adjacentCoordinates.Add(bottomNeighbour); 
+            adjacentCoordinates.Add(coordinate with {Y = coordinate.Y + 1});
         }
 
         return adjacentCoordinates;

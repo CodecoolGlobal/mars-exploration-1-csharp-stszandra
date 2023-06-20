12e86788a3431179be2ace7d0eafd9f6c8ec6bd8
fehervendel <fehervendel@gmail.com>
2023-06-20 09:55:11 +0200

    feature: start CoordinateCalculator, add GetRandomCoordinate

---
+++ Codecool.MarsExploration/Calculators/Service/CoordinateCalculator.cs
@@ -0,0 +1,25 @@
+﻿using Codecool.MarsExploration.Calculators.Model;
+
+namespace Codecool.MarsExploration.Calculators.Service;
+
+public class CoordinateCalculator : ICoordinateCalculator
+{
+    public Coordinate GetRandomCoordinate(int dimension)
+    {
+        Random random = new Random();
+        int randomXCoordinate = random.Next(0, dimension + 1);
+        int randomYCoordinate = random.Next(0, dimension + 1);
+        Coordinate coordinate = new Coordinate(randomXCoordinate, randomYCoordinate);
+        return coordinate;
+    }
+
+    public IEnumerable<Coordinate> GetAdjacentCoordinates(Coordinate coordinate, int dimension)
+    {
+        
+    }
+
+    public IEnumerable<Coordinate> GetAdjacentCoordinates(IEnumerable<Coordinate> coordinates, int dimension)
+    {
+        
+    }
+}

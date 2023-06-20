a3f81d24a35f19d4eb1eceec9e825d2492f104a3
thebortnyak <zsombor.bortnyak@gmail.com>
2023-06-20 14:13:52 +0200

    Map.cs tests

---
+++ MarsExplorationTests/MapTest.cs
@@ -0,0 +1,41 @@
+﻿using Codecool.MarsExploration.MapElements.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsExplorationTests
+{
+    public class MapTest
+    {
+        [Test]
+        public void CreateStringRepresentation_Should_Return_Correct_String()
+        {
+            // Arrange
+            string?[,] representation = new string?[2, 2] { { "A", "B" }, { "C", "D" } };
+            Map map = new Map(representation);
+
+            // Act
+            string result = map.ToString();
+
+            // Assert
+            string expected = "A B \nC D \n";
+            Assert.That(expected, Is.EqualTo(result));
+        }
+        [Test]
+        public void CreateStringRepresentation_Should_Handle_Null_Values()
+        {
+            // Arrange
+            string?[,] representation = new string?[2, 2] { { null, "B" }, { "C", null } };
+            Map map = new Map(representation);
+
+            // Act
+            string result = map.ToString();
+
+            // Assert
+            string expected = "null B \nC null \n";
+            Assert.That(expected,Is.EqualTo(result));
+        }
+    }
+}

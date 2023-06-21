13374b797661455c2eb1b78c3a488de581d9df32
stszandra <snszandra@gmail.com>
2023-06-21 10:32:14 +0200

    feature: implement CanPlaceElement method in MapElementPlacer

---
+++ Codecool.MarsExploration/MapElements/Service/Placer/MapElementPlacer.cs
@@ -0,0 +1,28 @@
+﻿using Codecool.MarsExploration.Calculators.Model;
+using Codecool.MarsExploration.MapElements.Model;
+
+namespace Codecool.MarsExploration.MapElements.Service.Placer;
+
+public class MapElementPlacer : IMapElementPlacer
+{
+    public bool CanPlaceElement(MapElement element, string?[,] map, Coordinate coordinate)
+    {
+        int dimension = element.Dimension;
+        for (int y = coordinate.Y; y < coordinate.Y + dimension - 1; y++)
+        {
+            for (int x = coordinate.X; x < coordinate.X + dimension - 1; x++)
+            {
+                if (map[y, x] != null)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void PlaceElement(MapElement element, string?[,] map, Coordinate coordinate)
+    {
+        
+    }
+}
